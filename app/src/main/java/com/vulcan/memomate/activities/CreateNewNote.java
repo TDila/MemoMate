459477856d292f1d2df68c57426f0d5e40aec398
Tharusha Dilhara <tharushadilhara17@gmail.com>
2024-05-22 23:00:23 +0530

    Designed the creating a new note activity

--- app/src/main/java/com/vulcan/memomate/activities/CreateNewNote.java
+++ app/src/main/java/com/vulcan/memomate/activities/CreateNewNote.java
@@ -1,14 +1,16 @@
-package com.vulcan.memomate;
+package com.vulcan.memomate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.vulcan.memomate.R;
+
+public class CreateNewNote extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_new_note);
     }
 }
---
+++ app/src/main/java/com/vulcan/memomate/activities/MainActivity.java
@@ -0,0 +1,26 @@
+package com.vulcan.memomate.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.vulcan.memomate.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.addNoteBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CreateNewNote.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
